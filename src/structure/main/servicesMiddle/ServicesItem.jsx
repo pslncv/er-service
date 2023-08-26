cfe75dc8914e5fd1021e88d1413e3ed1abe9006f
pslncv <as.poselentsev@gmail.com>
2023-08-26 23:28:05 +0300

    v0.3.1 Added buttons to middle service section

--- src/structure/main/servicesMiddle/ServicesItem.jsx
+++ src/structure/main/servicesMiddle/ServicesItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const ServiceItem = (props) => {
-    return ( 
-        <div className='services__list_middle-item'>
-            <span>{props.service.title}</span>
-        </div>
+    return (
+        <>        
+            <div className='services__list_middle-item'>
+                <div className='services__list_middle-title'>
+                    <span>{props.service.title}</span>
+                </div>
+                <button className='services__list_middle-button'>
+                    <span>Заказать</span>
+                </button>
+            </div>
+        </>
     );
 }
  
