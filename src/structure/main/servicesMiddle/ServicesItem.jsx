6178e1f1f7742b94e388ec07efe22dd1f3771eb0
pslncv <as.poselentsev@gmail.com>
2023-08-28 10:30:21 +0300

    v0.3.2 Added prices to middle service section

--- src/structure/main/servicesMiddle/ServicesItem.jsx
+++ src/structure/main/servicesMiddle/ServicesItem.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const ServiceItem = (props) => {
-    return (
-        <>        
-            <div className='services__list_middle-item'>
-                <div className='services__list_middle-title'>
-                    <span>{props.service.title}</span>
-                </div>
-                <button className='services__list_middle-button'>
-                    <span>Заказать</span>
-                </button>
+    return (  
+        <div className='services__list_middle-item'>
+            <div className={`services__list_middle-title-${props.service.id}`}>
+                <span className='services__list_middle-name'>{props.service.title}</span>
+                <span className='services__list_middle-price'>{props.service.price}</span>
             </div>
-        </>
+            <button className='services__list_middle-button'>
+                <span>Заказать</span>
+            </button>
+        </div>
     );
 }
  
--- src/structure/main/servicesMiddle/ServicesListMiddle.jsx
+++ src/structure/main/servicesMiddle/ServicesListMiddle.jsx
@@ -4,12 +4,12 @@ import ServicesItem from '../servicesMiddle/ServicesItem';
 const ServicesListMiddle = () => {
 
     const [services] = useState ([
-        {id: 1, title: 'Установка смесителя',},
-        {id: 2, title: 'Проверка счетчиков',},
-        {id: 3, title: 'Клининг квартиры',},
-        {id: 4, title: 'Установка кондиционера',},
-        {id: 5, title: 'Магнитный ключ',},
-        {id: 6, title: 'Повесить\nполку',}
+        {id: 1, title: 'Установка смесителя', price: '500 р'},
+        {id: 2, title: 'Проверка счетчиков', price: '700 р'},
+        {id: 3, title: 'Клининг квартиры', price: '900 р'},
+        {id: 4, title: 'Установка кондиционера', price: '1500 р'},
+        {id: 5, title: 'Магнитный ключ', price: '500 р'},
+        {id: 6, title: 'Повесить\nполку', price: '500 р'}
     ])
 
     return ( 
